694330dc890ab7f9df0a777b3d0efa2d2a19103c
gdelrio87 <gdelrioala@gmail.com>
2018-03-07 12:40:37 +0100

    EPD4 - p2 - funciona a falta de tutoría

---
+++ EPD4/p2/src/java/Modelo/Aparcamiento.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Antonio Jose Herrera Tabaco
+ */
+public class Aparcamiento {
+
+    private static final String controlador = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost/aparcamiento";
+    private static final String usuario = "root";
+    private static final String clave = "";
+
+    static {
+        try {
+            Class.forName(controlador);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error loading JDBC driver"
+                    + " class. Cause: " + e);
+        }
+
+    }
+
+    public static List<Coche> listadoCoches() throws SQLException {
+        List<Coche> listaCoches = new ArrayList<Coche>();
+        
+        Connection conexion = DriverManager.getConnection(url,usuario,clave);
+        
+        Statement st = (Statement) conexion.createStatement();
+        
+        ResultSet resultado = st.executeQuery("select * from coche");
+
+        while(resultado.next()){
+        Coche coche = new Coche(resultado.getString("matricula"), resultado.getString("modelo"), resultado.getTime("horaEntrada"), resultado.getTime("horaSalida"), resultado.getInt("tiempoPermitido"));
+        listaCoches.add(coche);
+        }
+        
+        return listaCoches;
+    }
+}
---
+++ EPD4/p2/src/java/Modelo/Coche.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Time;
+
+/**
+ *
+ * @author Antonio Jose Herrera Tabaco
+ */
+public class Coche {
+
+    private String matricula;
+    private String modelo;
+    private Time horaEntrada;
+    private Time horaSalida;
+    private int tiempoPermitido;
+
+    public Coche(String matricula, String modelo, Time horaEntrada, Time horaSalida, int tiempoPermitido) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.horaEntrada = horaEntrada;
+        this.horaSalida = horaSalida;
+        this.tiempoPermitido = tiempoPermitido;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Time getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(Time horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public Time getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(Time horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public int getTiempoPermitido() {
+        return tiempoPermitido;
+    }
+
+    public void setTiempoPermitido(int tiempoPermitido) {
+        this.tiempoPermitido = tiempoPermitido;
+    }
+
+    public boolean esSuperado() {
+        boolean superado = false;
+        if ((this.getHoraEntrada().getTime() + this.getTiempoPermitido() * 60000) < this.getHoraSalida().getTime()) {
+            superado = true;
+        }
+        return superado;
+    }
+}
