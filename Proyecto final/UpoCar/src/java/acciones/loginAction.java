76b4219c264c91482589b99650ad91d09d4dad06
gdelrio87 <gdelrioala@gmail.com>
2018-04-27 06:01:40 +0200

    Proyecto: Creación de proyecto con struts + hibernate

---
+++ Proyecto final/UpoCar/src/java/acciones/loginAction.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acciones;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Gonza
+ */
+public class loginAction extends ActionSupport {
+    
+    public loginAction() {
+    }
+    
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+    
+}
