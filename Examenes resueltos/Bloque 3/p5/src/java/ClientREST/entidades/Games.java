e71023d701063ad000f5bb9d034420553c251252
gdelrio87 <gdelrioala@gmail.com>
2018-05-19 18:33:50 +0200

    Bloque 3 - Examen

---
+++ Examenes resueltos/Bloque 3/p5/src/java/ClientREST/entidades/Games.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClientREST.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Gonza
+ */
+@Entity
+@Table(name = "games")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Games.findAll", query = "SELECT g FROM Games g")
+    , @NamedQuery(name = "Games.findById", query = "SELECT g FROM Games g WHERE g.id = :id")
+    , @NamedQuery(name = "Games.findByYear", query = "SELECT g FROM Games g WHERE g.year = :year")})
+public class Games implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "console")
+    private String console;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "year")
+    @Temporal(TemporalType.DATE)
+    private Date year;
+
+    public Games() {
+    }
+
+    public Games(Integer id) {
+        this.id = id;
+    }
+
+    public Games(Integer id, String name, String console, Date year) {
+        this.id = id;
+        this.name = name;
+        this.console = console;
+        this.year = year;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConsole() {
+        return console;
+    }
+
+    public void setConsole(String console) {
+        this.console = console;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Games)) {
+            return false;
+        }
+        Games other = (Games) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientREST.entidades.Games[ id=" + id + " ]";
+    }
+    
+}
